f6c8fc719307d72f0d938d0c55ea0d65e47fc6ca
zjuws <315876632@qq.com>
2022-06-03 16:15:52 +0800

    grid layout

--- pyqt.py
+++ pyqt.py
@@ -2,7 +2,7 @@ import sys
 from PyQt6.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import (QWidget, QMainWindow, QLineEdit, QLabel,
-                             QHBoxLayout, QVBoxLayout, QPushButton, QApplication)
+                             QHBoxLayout, QVBoxLayout, QPushButton, QApplication, QGridLayout)
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -21,35 +21,36 @@ class MyWidget(QWidget):
         self.initUI()
 
     def initUI(self):
+        grid = QGridLayout()
+        grid.setSpacing(10)
 
+        path = QLabel('文件夹路径')
+        rep = QLabel('替换版本')
+        pathEdit = QLineEdit()
+        pathEdit.textChanged[str].connect(self.onChanged)
+        repEdit = QLineEdit()
+        grid.addWidget(path, 1, 0)
+        grid.addWidget(pathEdit, 1, 1)
 
-        self.lbl = QLabel(self)
-        qle = QLineEdit(self)
-        qle.textChanged[str].connect(self.onChanged)
-        hbox = QVBoxLayout()
-        hbox.addWidget(self.lbl)
-        hbox.addWidget(qle)
+        grid.addWidget(rep, 2, 0)
+        grid.addWidget(repEdit, 2, 1)
 
         button1 = QPushButton('一键替换')
-        button2 = QPushButton('退出')
-        hbox2 = QHBoxLayout()
-        hbox2.addStretch(1)
-        hbox2.addWidget(button2)
-        hbox2.addWidget(button1)
+        qbtn = QPushButton('退出')
+        qbtn.clicked.connect(QApplication.instance().quit)
+        hbox = QHBoxLayout()
+        hbox.addWidget(qbtn)
+        hbox.addWidget(button1)
+        grid.addLayout(hbox, 3,1)
 
-
-        vbox = QVBoxLayout()
-        vbox.addLayout(hbox)
-        vbox.addLayout(hbox2)
-
-        self.setLayout(vbox)
+        self.setLayout(grid)
         # self.setGeometry(300, 300, 350, 250)
         # self.setWindowTitle('My App')
         # self.show()
     def onChanged(self, text):
-
-        self.lbl.setText(text)
-        self.lbl.adjustSize()
+        a = text
+        # self.lbl.setText(text)
+        # self.lbl.adjustSize()
 
 def main():
     app = QApplication([])
